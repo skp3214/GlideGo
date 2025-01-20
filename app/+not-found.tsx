22922746a48eef80bb5742a1665d6ac75fc75611
Sachin Prajapati <skprajapati3214@gmail.com>
2025-01-21 02:48:15 +0530

    add initial layout and routing for authentication and main app screens

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { Link, Stack } from 'expo-router';
+import { StyleSheet, Text } from 'react-native';
+import { View } from 'react-native';
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops!' }} />
+      <View style={styles.container}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>This screen doesn't exist.</Text>
+        <Link href="/" style={styles.link}>
+          <Text style={{ color: 'blue' }}>Go to home screen!</Text>
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  link: {
+    marginTop: 15,
+    paddingVertical: 15,
+  },
+});
